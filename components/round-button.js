dd7c06dc3b387284de0ef0317494a26a0ccdc91f
James <james.a.forbes@gmail.com>
2016-12-19 14:03:26 +1100

    Remove old js artifact

--- components/round-button.js
+++
@@ -1,9 +0,0 @@
-var roundButton, out$ = typeof exports != 'undefined' && exports || this;
-out$.roundButton = roundButton = h(function(){
-  return function(text, arg$){
-    var bg, ref$;
-    text == null && (text = '+');
-    bg = (ref$ = arg$.bg) != null ? ref$ : 'blue';
-    return h('button', h('span', text));
-  };
-});
